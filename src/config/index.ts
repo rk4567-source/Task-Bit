2592c498f38850f798578d5e6c6606af0ac2601d
Rockey kumar <rp0205173@gmail.com>
2026-04-18 23:51:46 +0530

    initial commit

---
+++ src/config/index.ts
@@ -0,0 +1,56 @@
+import type { Metadata } from 'next';
+
+export const siteConfig: Metadata = {
+  title: 'TaskBit',
+  description: 'Full-stack Task Management App using Next.js 14 and Appwrite.',
+  keywords: [
+    'reactjs',
+    'nextjs',
+    'appwrite',
+    'appwrite-io',
+    'next-auth',
+    'react-big-calendar',
+    'lucide-icons',
+    'react-icons',
+    'react-day-picker',
+    'shadcn-ui',
+    'radix-ui',
+    'tailwindcss',
+    'lodash',
+    'react-query',
+    'nuqs',
+    'sonner',
+
+    'typescript',
+    'javascript',
+    'vercel',
+    'postcss',
+    'prettier',
+    'eslint',
+    'react-dom',
+    'html',
+    'css',
+
+    'nuqs-state-management',
+    'infinite-query',
+    'real-time-features',
+    'organized-table',
+    'filter-table',
+    'paginate-table',
+    'tasks-management',
+    'projects-management',
+    'workspaces-management',
+    'ui/ux',
+    'appwrite-db',
+    'TaskBit',
+
+    'date-fns',
+    'cn',
+    'clsx',
+    'lucide-react',
+  ] as Array<string>,
+} as const;
+
+export const links = {
+  sourceCode: 'https://github.com/rk4567/TaskBit',
+} as const;
